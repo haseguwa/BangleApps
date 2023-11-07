4ca29e5c7720ac491272c474f0d5f93664d5f654
haseguwa <haseguwa@gmail.com>
2023-11-07 23:29:48 +0900

    Update app.js
    rewrite the code to avoid setInterval
    return to clock after the vibration finished

--- apps/minutesviber/app.js
+++ apps/minutesviber/app.js
@@ -4,7 +4,7 @@ const QUARTER_VIBRATION = 300;
 const QUARTER_INTERVAL = 500;
 const REMAINING_VIBRATION = 100;
 const REMAINING_INTERVAL = 300;
-const SEPARATOR_INTERVAL = 400;
+const SEPARATOR_INTERVAL = 300;
 
 var currentDate = new Date();
 var hour = (currentDate.getHours() - 1) % 12 + 1;
@@ -16,41 +16,33 @@ var rmin = min % 15;
  // for debug
 hour = 2;
 qmin = 1;
-rmin = 4;
+rmin = 14;
 */
 
-// Hour vibration
-Bangle.buzz(HOUR_VIBRATION, 1);
-hour -= 1;
-var hid = setInterval(function() {
+function vibe() {
   if (hour > 0) {
+    if (hour == 1) {
+      setTimeout(vibe, HOUR_INTERVAL + SEPARATOR_INTERVAL);
+    } else {
+      setTimeout(vibe, HOUR_INTERVAL);
+    }
     Bangle.buzz(HOUR_VIBRATION, 1);
     hour -= 1;
+  } else if (qmin > 0) {
+    if (qmin == 1) {
+      setTimeout(vibe, QUARTER_INTERVAL + SEPARATOR_INTERVAL, 1);
+    } else {
+      setTimeout(vibe, QUARTER_INTERVAL, 1);
+    }
+    Bangle.buzz(QUARTER_VIBRATION, 1);
+    qmin -= 1;
+  } else if (rmin > 0) {
+    setTimeout(vibe, REMAINING_INTERVAL, 1);
+    Bangle.buzz(REMAINING_VIBRATION, 1);
+    rmin -= 1;
   } else {
-    clearInterval(hid);
+    load();
   }
-}, HOUR_INTERVAL);
+}
 
-// Quarter vibration
-setTimeout(function() {
-  var qid = setInterval(function() {
-    if (qmin > 0) {
-      Bangle.buzz(QUARTER_VIBRATION, 1);
-      qmin -= 1;
-    } else {
-      clearInterval(qid);
-    }
-  }, QUARTER_INTERVAL);
-}, HOUR_INTERVAL * hour);
-
-// Remaining vibration
-setTimeout(function() {
-  var rid = setInterval(function() {
-    if (rmin > 0) {
-      Bangle.buzz(REMAINING_VIBRATION, 1);
-      rmin -= 1;
-    } else {
-      clearInterval(rid);
-    }
-  }, REMAINING_INTERVAL);
-}, HOUR_INTERVAL * hour + QUARTER_INTERVAL * qmin + SEPARATOR_INTERVAL);
+vibe();
