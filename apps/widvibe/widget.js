314e46585571bfe7f9d4a1a3bb625eabf6828c09
haseguwa <haseguwa@gmal.com>
2023-11-24 11:37:20 +0900

    add autozone app

--- apps/widvibe/widget.js
+++ apps/widvibe/widget.js
@@ -26,11 +26,13 @@
   
   function draw() {
     g.setColor(0x07ff);
-    g.drawImage(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="),2+this.x,2+this.y);
+    g.drawImage(atob("DBgBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), this.x + 2, this.y + 2);
+//    g.drawImage(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="),2+this.x,2+this.y);
   }
   
   WIDGETS.viber = { area: "tl", width: 15, draw: draw };
 
+/*
   setWatch(function() {
     if (!running && Bangle.CLOCK == 1) {
       running = true; 
@@ -42,8 +44,7 @@
       vibe(hour, qmin, rmin);
     }
   }, BTN, {repeat:true });
-
-/*
+*/
   Bangle.on('touch', function(button, xy) {
     if (!running && Bangle.CLOCK == 1) {
       running = true;
@@ -55,5 +56,4 @@
       vibe(hour, qmin, rmin);
     }
   });
-*/
 })();
