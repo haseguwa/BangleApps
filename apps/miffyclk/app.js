15b6d858e31b841effa6766fc73ac3305dae8ca1
haseguwa <haseguwa@gmal.com>
2023-12-09 17:29:47 +0900

    add miffy clock

--- apps/miffyclk/app.js
+++ apps/miffyclk/app.js
@@ -3,6 +3,10 @@
     width : 176, height : 152, bpp : 1,
     buffer : require("heatshrink").decompress(atob("ADMD+P4AgMfw//wAFBg/gCqHACqgRCCpUAuE4DQQ/BCIYV/CqODNoYVQTIgVQx7FDCqGOBQgV/NqaZTCswA/AH0fwCQECp+B//wCqWD//4CR0BHoMDw+AngVQ+DoBgFeCqH4geOgPuKyE4CoMDCqkP5wVTj8OCqd+CqBtD/AVPTIeO+AVPgbFCx9AjwVPwACBbaIVDwZbBCqWBLYIVSwBbBNqA9PCt4A/AH4A/AH4A/AH4A/AHkH/wSQgf/8EP/4VS+Ef/+ACqP4jkD4BWRvEcgIVSnAVJgfgCqcH+AVJNoIVHg0wNpKZBCqcHCqkAK6gVJQZYVJV5YiBbaZtBwATPQYgpQCoPwg/+CqR3JAH4A/AH4A/AH4A/AH4A/AH4AIgP8CSEDwE4g//CtMPg+ACqUOAYIVoIKptT//4h//FaIVBj//4AVSjkBCqAABnAV/CoJtSCoSZSCoTFSCoQTSAH4AJ//8AgUf/AECv/+CpQLDn4VUv4VPgE8AYXwBAdwWx/gCqF4Cqg9DCqv/NoijDAA0BE4a0En6DKgPATw8f/4VJgYVD8EOBQcHCpMHwAVph6HECp4PEv5tDv5tKjwEDn6ZDDQgAGngEDj7FPAHgA=="))
   };
+  const miffy = {
+    width : 30, height : 45, bpp : 1,
+    buffer : require("heatshrink").decompress(atob("ABPggPgg/AAIM/gE/wH8gH+g/4gf8j/gh/wv+An/D/0A/8PBwP+n4OB/H/BwPjBwWPBw8Aj/jFgN/x/wgP///Agf/DIMP/4OC/4dBBwPwj4DB8ACB//DAYQKC/wVB//5BQXnAYWfBwV+v/5AAINBBw38n4wCEIWB/8n/xZC+ED/V/gEHIwIDCTxA"))
+  };
 
   const rad = Math.PI / 30;
   const offset = [[-1,-2],[0,-2],[1,-2],[-2,-1],[-1,-1],[1,-1],[2,-1],[-2,0],[2,0],[-2,1],[-1,1],[1,1],[2,1],[-1,2],[0,2],[1,2]];
@@ -39,6 +43,10 @@
     g.drawString(require('locale').dow(date).substr(0,3).toUpperCase(), 130, 95);
     g.drawString(date.getDate(), 130,105);
 
+    g.setColor(g.getBgColor() ^ 0x1F);
+    g.drawImage(miffy, 73, 105);
+  
+    g.setColor(g.getBgColor() ^ 0xffff);
     drawhand((date.getHours() - 3) * 5 + date.getMinutes() / 12, 12, 50);
     g.setColor(((g.getBgColor() & 0x7E0) ^ 0x7E0) | 0x1F);
     drawhand(date.getMinutes() - 15, 12, 65);
