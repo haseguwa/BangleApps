bdffa0de9ae73372765d2d737e92807469e9596f
haseguwa <haseguwa@gmal.com>
2024-01-08 11:49:15 +0900

    rewrite using widcal

--- apps/widcalr/widget.js
+++ apps/widcalr/widget.js
@@ -1,38 +1,25 @@
 (() => {
   require("FontTeletext5x9Mode7").add(Graphics);
   require("Font7x11Numeric7Seg").add(Graphics);
+  WIDGETS.calr = {
+    area: "tr", width: 22, draw: function() {
+      const x = this.x, y = this.y,
+        x2 = x+20, y2 = y+22,
+        date = new Date(),
+        month = require("locale").month(date, true),
+        day = date.getDate();
 
-  function draw() {
-    const x = this.x;
-    const y = this.y;
-    const x2 = x + 18;
-    const date = new Date();
-    const month = require("locale").month(date, true);
-    const day = date.getDate();
-
-    g.reset();
-    g.setFontAlign(0, 0, 0);
-    const fgcolor = g.getColor();
-    const bgcolor = g.getBgColor();
-
-    g.setColor(1,0,0);
-    g.fillRect(x, y, x2, y + 7);
-    g.setColor(1,1,1);
-    g.setFont("Teletext5x9Mode7");
-    g.drawString(month, x + 10, y + 4);
-
-    g.setColor(fgcolor);
-    g.fillRect(x, y + 8, x2, y + 22);
-    g.setColor(bgcolor);
-    g.setFont("7x11Numeric7Seg");
-    g.drawString(day, x + 10, y + 15);
-
-    if (WIDGETS.calr.to) {
-      clearTimeout(WIDGETS.calr.to);
+      g.reset().setFontAlign(0, 0); // center all text
+      const fgcolor = g.getColor();
+        // header
+      g.setBgColor("#f00").setColor("#fff")
+        .clearRect(x, y, x2, y+7).setFont("Teletext5x9Mode7").drawString(month, x+11, y+4)
+        // date
+        .setBgColor(fgcolor).setColor(fgcolor ^ 0xFFFF)
+        .clearRect(x, y+8, x2, y2).setFont("7x11Numeric7Seg").drawString(day, x+11, y+15);
+      // redraw when date changes
+      if (WIDGETS.calr.to) clearTimeout(WIDGETS.calr.to);
+      WIDGETS.calr.to = setTimeout(()=>WIDGETS.calr.draw(), (86401 - Math.floor(date/1000) % 86400)*1000);
     }
-    WIDGETS.calr.to = setTimeout(draw, (86400000 - date) % 86400000);
-  }
-
-  WIDGETS.calr = { area: "tr", width: 20, draw: draw };
-
+  };
 })();
