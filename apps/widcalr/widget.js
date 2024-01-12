355e24da803656bca7963c4fcf9ed707047d7dcc
haseguwa <haseguwa@gmal.com>
2024-01-12 23:32:00 +0900

    change font

--- apps/widcalr/widget.js
+++ apps/widcalr/widget.js
@@ -1,6 +1,7 @@
 (() => {
   require("FontTeletext5x9Mode7").add(Graphics);
-  require("Font7x11Numeric7Seg").add(Graphics);
+  require("Font8x16").add(Graphics);
+
   WIDGETS.calr = {
     area: "tr", width: 22, draw: function() {
       const x = this.x, y = this.y,
@@ -9,17 +10,18 @@
         month = require("locale").month(date, true),
         day = date.getDate();
 
-      g.reset().setFontAlign(0, 0); // center all text
+      g.reset().setFontAlign(0, 0);
       const fgcolor = g.getColor();
-        // header
+
       g.setBgColor("#f00").setColor("#fff")
-        .clearRect(x, y, x2, y+7).setFont("Teletext5x9Mode7").drawString(month, x+11, y+4)
-        // date
+        .clearRect(x, y, x2, y+8).setFont("Teletext5x9Mode7").drawString(month, x+11, y+5)
         .setBgColor(fgcolor).setColor(fgcolor ^ 0xFFFF)
-        .clearRect(x, y+8, x2, y2).setFont("7x11Numeric7Seg").drawString(day, x+11, y+15);
-      // redraw when date changes
-      if (WIDGETS.calr.to) clearTimeout(WIDGETS.calr.to);
-      WIDGETS.calr.to = setTimeout(()=>WIDGETS.calr.draw(), (86401 - Math.floor(date/1000) % 86400)*1000);
+        .clearRect(x, y+9, x2, y2).setFont("8x16").drawString(day, x+11, y+17);
+
+      if (WIDGETS.calr.to) {
+        clearTimeout(WIDGETS.calr.to);
+      }
+      WIDGETS.calr.to = setTimeout(()=>WIDGETS.calr.draw(), ((86400000 - date) % 86400000));
     }
   };
 })();
