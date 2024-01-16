fe68a8d4536f28bb4f3f1dfe83246672b56956e5
haseguwa <haseguwa@gmal.com>
2024-01-16 23:30:25 +0900

    fix timeout bug

--- apps/widcalr/widget.js
+++ apps/widcalr/widget.js
@@ -1,27 +1,39 @@
 (() => {
   require("FontTeletext5x9Mode7").add(Graphics);
   require("Font8x16").add(Graphics);
+  let drawTimeout;
 
   WIDGETS.calr = {
     area: "tr", width: 22, draw: function() {
-      const x = this.x, y = this.y,
-        x2 = x+20, y2 = y+22,
-        date = new Date(),
-        month = require("locale").month(date, true),
-        day = date.getDate();
+      const x = this.x;
+      const y = this.y;
+      const date = new Date();
+      const month = require("locale").month(date, true);
+      const day = date.getDate();
 
-      g.reset().setFontAlign(0, 0);
+      g.reset();
+      g.setFontAlign(0, 0);
       const fgcolor = g.getColor();
 
-      g.setBgColor("#f00").setColor("#fff")
-        .clearRect(x, y, x2, y+8).setFont("Teletext5x9Mode7").drawString(month, x+11, y+5)
-        .setBgColor(fgcolor).setColor(fgcolor ^ 0xFFFF)
-        .clearRect(x, y+9, x2, y2).setFont("8x16").drawString(day, x+11, y+17);
+      g.setBgColor(1, 0, 0);
+      g.setColor(1, 1, 1);
+      g.clearRect(x, y, x + 20, y + 8);
+      g.setFont("Teletext5x9Mode7");
+      g.drawString(month, x + 11, y + 5);
 
-      if (WIDGETS.calr.to) {
-        clearTimeout(WIDGETS.calr.to);
+      g.setBgColor(fgcolor);
+      g.setColor(fgcolor ^ 0xFFFF);
+      g.clearRect(x, y + 9, x + 20, y + 22);
+      g.setFont("8x16");
+      g.drawString(day, x + 11, y + 17);
+
+      if (drawTimeout) {
+        clearTimeout(drawTimeout);
       }
-      WIDGETS.calr.to = setTimeout(()=>WIDGETS.calr.draw(), ((86400000 - date) % 86400000));
+      drawTimeout = setTimeout(() => {
+        drawTimeout = undefined;
+        WIDGETS.calr.draw();
+      }, 86400000 - Date.now() % 86400000);
     }
   };
 })();
