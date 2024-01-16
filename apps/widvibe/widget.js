2c8a7f40823c3b7d920a62c37bbf226fff6efce8
haseguwa <haseguwa@gmal.com>
2024-01-16 23:16:24 +0900

    simplify the code

--- apps/widvibe/widget.js
+++ apps/widvibe/widget.js
@@ -1,23 +1,16 @@
 (() => {
-  const hour_vibration = 150;
-  const hour_interval = 300;
-  const quarter_vibration = 300;
-  const quarter_interval = 500;
-  const remain_vibration = 100;
-  const remain_interval = 300;
-  const separator_interval = 300;
   let running = false;
 
-  let vibe = (hour, qmin, rmin) => {
+  const vibe = (hour, qmin, rmin) => {
     if (hour > 0) {
-      setTimeout(vibe, hour_interval + (hour == 1 ? separator_interval : 0), hour - 1, qmin, rmin);
-      Bangle.buzz(hour_vibration, 1);
+      setTimeout(vibe, 300 + (hour == 1 ? 300 : 0), hour - 1, qmin, rmin);
+      Bangle.buzz(150, 1);
     } else if (qmin > 0) {
-      setTimeout(vibe, quarter_interval + (qmin == 1 ? separator_interval : 0), hour, qmin - 1, rmin);
-      Bangle.buzz(quarter_vibration, 1);
+      setTimeout(vibe, 500 + (qmin == 1 ? 300 : 0), 0, qmin - 1, rmin);
+      Bangle.buzz(300, 1);
     } else if (rmin > 0) {
-      setTimeout(vibe, remain_interval, hour, qmin, rmin - 1);
-      Bangle.buzz(remain_vibration, 1);
+      setTimeout(vibe, 300, 0, 0, rmin - 1);
+      Bangle.buzz(100, 1);
     } else {
       running = false;
     }
@@ -32,8 +25,8 @@
   Bangle.on('touch', (button, xy) => {
     if (!running && Bangle.CLOCK == 1) {
       running = true;
-      let date = new Date();
-      let min = date.getMinutes();
+      const date = new Date();
+      const min = date.getMinutes();
       vibe((date.getHours() + 11) % 12 + 1, Math.floor(min / 15), min % 15);
     }
   });
